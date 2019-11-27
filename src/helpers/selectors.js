8b360051096785c077392f2f0f5d5bffca151ded
morbi <the_morbidus@hotmail.com>
2019-11-27 17:36:00 -0500

    selectors and selectors.test added

---
+++ src/helpers/selectors.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function getAppointmentsForDay(state, day) {
+  const days = state.days;                  
+  const appointments = state.appointments;  
+  if (days.length === 0) {
+    return []
+  }
+
+  for (let i = 0; i < days.length; i++) { 
+    if (days[i].name === day) { 
+      let appointmentsArray = [];
+      let daysArray = days[i].appointments;
+      for (let j = 0; j < daysArray.length; j++) {
+        let keysArray = Object.keys(appointments);
+        let singleDay = daysArray[j].toString();
+        if (keysArray.includes(singleDay)) {
+          appointmentsArray.push(appointments[singleDay]);
+        }
+      }
+      return appointmentsArray;
+    }
+  }
+  return [];
+}
