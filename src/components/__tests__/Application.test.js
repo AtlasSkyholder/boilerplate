5f5037ae0bc952977912320f88f5250a803076f3
morbi <the_morbidus@hotmail.com>
2019-12-02 18:17:28 -0500

    Implementation of async/await tests

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,11 +1,18 @@
 import React from "react";
 
-import { render, cleanup } from "@testing-library/react";
+import { render, cleanup, fireEvent, getByText, waitForElement } from "@testing-library/react";
 
 import Application from "components/Application";
 
+import axios from "axios";
+
 afterEach(cleanup);
 
-it.skip("renders without crashing", () => {
-  render(<Application />);
+
+it("defaults to Monday and changes the schedule when a new day is selected", async () => {
+  const { getByText } = render(<Application />);
+
+  await waitForElement(() => getByText("Monday"));
+  fireEvent.click(getByText("Tuesday"))
+  expect(getByText("Leopold Silvers")).toBeInTheDocument();
 });
