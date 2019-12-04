4f83b86f890249c0c586ebcc5e20856030b8b98e
morbi <the_morbidus@hotmail.com>
2019-12-03 23:49:02 -0500

    Cypress tests implementation

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -18,7 +18,7 @@ describe("Application", () => {
     expect(getByText("Leopold Silvers")).toBeInTheDocument();
   });
   
-  it.skip("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
+  it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
 
     const { container, debug } = render(<Application />);
 
@@ -36,7 +36,7 @@ describe("Application", () => {
     fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
     fireEvent.click(getByText(appointment, "Save"));
   
-    expect(getByText(appointment, "Saving")).toBeInTheDocument();
+    expect(getByText(appointment, "SAVING")).toBeInTheDocument();
   
     await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
   
@@ -48,7 +48,7 @@ describe("Application", () => {
 
   });
 
-  it.skip("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
+  it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
     // 1. Render the Application.
     const { container, debug } = render(<Application />);
   
